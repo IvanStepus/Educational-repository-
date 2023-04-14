8bf5109bb2a3d9856d754c0112d1e9c8421cbdfb
IvanStepus <vanya-stepys@mail.ru>
2023-04-14 22:40:29 +0300

    The task description has been translated into English and an additional solution to the given problem has been added

--- functions.py
+++ functions.py
@@ -1,22 +1,43 @@
-'''Напишите функцию, которая принимает два аргумента - числа a и b, и возвращает другую функцию.
-Возвращаемая функция должна принимать один аргумент x и возвращать результат выражения a * x + b.'''
+'''Write a function that takes two arguments - numbers a and b - and returns another function.
+The returned function should take one argument x and return the result of the expression a * x + b.'''
 
 
 def numbers(a, b):
     def digit(x):
         return a * x + b
+
     return digit
+
+
 result = numbers(3, 5)
 print(result(2))
 
-'''Напишите функцию, которая принимает список чисел и возвращает другую функцию. 
-Возвращаемая функция должна принимать один аргумент - число n, и возвращать n-ый элемент списка.'''
+'''Write a function that takes a list of numbers and returns another function.
+The returned function should take one argument - a number n - and return the n-th element of the list.'''
 
 
 def main_func(sp):
     def digit(n):
         return sp[n - 1]
+
     return digit
+
+
 sp = [11, 22, 332, 412, 5312, 6321, 7123, 8123, 9112]
 result = main_func(sp)
 print(result(5))
+
+'''Write a function that takes one argument - a number n - and returns another function.
+The returned function should take one argument x and return the result of raising x to the power of n.'''
+
+
+def degree(n):
+    def arg(x):
+        return x ** n
+
+    return arg
+
+
+n = 2
+result = degree(n)
+print(result(3))
